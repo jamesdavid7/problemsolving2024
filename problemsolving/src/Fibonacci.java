966ab93ae1f8a8a05169f2aef35eef50f1547e04
jamesdavid7 <jaMes@1205>
2024-10-23 17:22:40 +0530

    Fibonacci problem Recursive function added

--- problemsolving/src/Fibonacci.java
+++ problemsolving/src/Fibonacci.java
@@ -27,8 +27,27 @@ public class Fibonacci {
         }
     }
 
+    public static void printFibonacciByRecursive(int prev2,int prev1,int count,int range) {
+        int fibo = prev2 + prev1;
+        System.out.println(fibo);
+        count = count+1;
+        if(count < range) {
+            prev2 = prev1;
+            prev1 = fibo;
+            printFibonacciByRecursive(prev2,prev1,count,range);
+        }
+    }
+
     public static void main(String[] args) {
 //        printFibonacciByRange(18);
-        printFibonacciByTimes(18);
+//        printFibonacciByTimes(18);
+
+
+
+        int prev2 = 0;
+        int prev1 = 1;
+        System.out.println(prev2);
+        System.out.println(prev1);
+        printFibonacciByRecursive(prev2,prev1,2,5);
     }
 }
